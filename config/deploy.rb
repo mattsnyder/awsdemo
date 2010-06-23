22ef8154236a087feaee50ee01693f75e086ffef
Matt Snyder <dixcom@msnyder.local>
2010-06-23 16:00:36 -0400

    added ssh identify key info to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,11 +2,14 @@ set :application, "awsdemo"
 set :repository,  "https://mattsnyder@github.com/mattsnyder/awsdemo.git"
 set :scm, :git
 
-set :deploy_to, "/Users/dixcom/Sites"
+set :deploy_to, "/var/www/rails-apps/awsdemo"
 
-set :use_sudo, false
+set :use_sudo, true
 
-server "awsdemo.local", :app, :web, :db, :primary => true
+server "184.72.252.0", :app, :web, :db, :primary => true
+
+ssh_options[:user] = "ubuntu"
+ssh_options[:keys] = [File.join(ENV['HOME'], '.ssh', 'id_apostate-AWS-Key')]
 
 namespace :deploy do
   task :start do ; end
